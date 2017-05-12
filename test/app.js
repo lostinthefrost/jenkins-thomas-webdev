2cc6d333d79df86673cfe4604cf68692b69a5d04
Thomas Jenkins <tjenkins@ccs.neu.edu>
2017-05-11 22:30:33 -0400

    Use production mongodb in production

--- test/app.js
+++ test/app.js
@@ -5,11 +5,8 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-        connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    if (process.env.MONGODB_URI) { // check if running remotely
+        connectionString = process.env.MONGODB_URI;
     }
 
     var mongoose = require("mongoose");
