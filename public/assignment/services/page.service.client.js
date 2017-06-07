523ec9df7c7cd3f49b89c32b2bf376e1b2781009
Thomas Jenkins <tjenkins@ccs.neu.edu>
2017-06-07 11:42:21 -0400

    Remove starter data from client services

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -5,12 +5,6 @@
 
     function PageService() {
 
-        var pages = [
-            { '_id': '321', 'name': 'Post 1', 'websiteId': '456', 'description': 'Lorem' },
-            { '_id': '432', 'name': 'Post 2', 'websiteId': '456', 'description': 'Lorem' },
-            { '_id': '543', 'name': 'Post 3', 'websiteId': '456', 'description': 'Lorem' }
-        ];
-
         return {
             createPage           : createPage,
             findPagesByWebsiteId : findPagesByWebsiteId,
--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -5,13 +5,6 @@
 
     function UserService($http) {
 
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
-
         return {
             createUser            : createUser,
             findUserById          : findUserById,
